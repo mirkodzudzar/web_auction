89794333a901f656570c8eebab1ea0b1ee086f7e
mirkodzudzar <mirkodzudzar@gmail.com>
2022-02-18 17:39:51 +0100

    Creating item-user migration with relationships for bidding data

---
+++ database/migrations/2022_02_18_162902_create_item_user_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_user', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('item_id');
+            $table->foreign('item_id')
+                  ->references('id')
+                  ->on('items');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->decimal('price', 10, 2);
+            $table->string('status')->default('active');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_user');
+    }
+}
