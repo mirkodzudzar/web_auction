eba87d0753bbdb031752d41fd6071a7dea917200
Mirko Džudžar <44711172+mirkodzudzar@users.noreply.github.com>
2022-11-10 09:41:56 +0100

    Delete ec2 directory

--- ec2/scripts/deploy_permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-APP_PATH="/var/www/web_auction"
-sudo find $APP_PATH -type d -exec chmod 777 {} +
-sudo find $APP_PATH -type f -exec chmod 777 {} +
-sudo chown -R ubuntu:ubuntu $APP_PATH/*
